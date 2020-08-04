883a845bbc845154f63bf2697dc3bfd94c97a8f9
Huskarhero <stevenjobs207@gmail.com>
2020-08-04 19:18:40 +0530

    Removing the dependency of css classes for icon styles in SocialMedia component by using styled-components

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import { ThemeProvider } from "styled-components";
 import { chosenTheme } from "./theme";
 import { GlobalStyles } from "./global";
 
+
 function App() {
+
   return (
-    <ThemeProvider theme={chosenTheme}>
-      <>
-        <GlobalStyles />
-        <div>
-          <Main theme={chosenTheme} />
-        </div>
-      </>
-    </ThemeProvider>
-  );
+		<ThemeProvider theme={chosenTheme}>
+			<>
+				<GlobalStyles />
+				<div>
+					<Main theme={chosenTheme} />
+				</div>
+			</>
+		</ThemeProvider>
+	);
 }
 
 export default App;
